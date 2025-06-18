08f8b24fcb86689230803b840ec0737e8b1d24cf
oshivimarsha <oshadivimarsha@gmail.com>
2025-06-18 09:54:35 +0530

    Update user service

---
+++ user-service/src/main/java/lk/ijse/userservice/config/WebSecurityConfig.java
@@ -0,0 +1,100 @@
+package lk.ijse.userservice.config;
+
+import lk.ijse.dreamnest_finalproject.service.impl.UserServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+
+@EnableWebSecurity
+@Configuration
+@EnableMethodSecurity(prePostEnabled = true)
+public class WebSecurityConfig {
+    @Autowired
+    private UserServiceImpl userService;
+    @Autowired
+    private JwtFilter jwtFilter;
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userService).passwordEncoder(passwordEncoder());
+    }
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration)
+            throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
+    }
+    @Bean
+    protected SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        return http
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers(
+                                "/api/v1/password/sentOTP",
+                                "/api/v1/password/resetPassword",
+                                "/api/v1/auth/authenticate",
+                                "/api/v1/user/**",
+                                "/api/v1/auth/refreshToken",
+                                "/api/v1/place/**",
+                                "/api/v1/hotel/**",
+                                "/api/v1/hotel/byPlace/{placeId}",
+                                "/api/v1/cab/**",
+                                "/api/v1/room/**",
+                                "/api/v1/room/byHotel/{hotel}",
+                                "/api/v1/roomType/**",
+                                "/api/v1/reviews/**",
+                                "/api/v1/HotelBooking/**",
+                                "/v3/api-docs/**",
+                                "/swagger-ui/**",
+                                "/swagger-ui.html").permitAll()
+                        .anyRequest().authenticated()
+                )
+                .sessionManagement(sess -> sess.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class)
+                .cors(Customizer.withDefaults())
+                .build();
+    }
+    @Bean
+    public CorsFilter corsFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials(true);
+        config.addAllowedOrigin("http://localhost:63342"); // Add your frontend origin here 63342
+        config.addAllowedHeader("*");
+        config.addAllowedMethod("*");
+        source.registerCorsConfiguration("/**", config);
+        return new CorsFilter(source);
+    }
+
+    @Configuration
+    public class WebConfig implements WebMvcConfigurer {
+        @Override
+        public void addResourceHandlers(ResourceHandlerRegistry registry) {
+            registry.addResourceHandler("/uploads/**")
+                    .addResourceLocations("file:uploads/");
+        }
+    }
+
+
+}
---
+++ user-service/src/main/java/lk/ijse/userservice/repo/UserRepository.java
@@ -0,0 +1,20 @@
+package lk.ijse.userservice.repo;
+
+import lk.ijse.userservice.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+
+public interface UserRepository extends JpaRepository<User,String> {
+
+    User findByEmail(String userName);
+
+    boolean existsByEmail(String userName);
+
+    int deleteByEmail(String userName);
+
+    @Modifying
+    @Query(value = "UPDATE user u SET u.password = ?2 WHERE u.username = ?1", nativeQuery = true)
+    void updatepassword(String name, String password);
+
+}
